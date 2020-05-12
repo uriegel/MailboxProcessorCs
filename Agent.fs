1de47158b189d3c7c0df84a0eef0cbdc7915ac8d
Uwe Riegel <uriegel@hotmail.de>
2020-05-12 12:26:04 +0200

    First commit

---
+++ Agent.fs
@@ -0,0 +1,16 @@
+﻿namespace MailboxProcessorCs
+
+open System
+
+type Agent<'a>(onMsg: Action<'a>) = 
+    let agent = MailboxProcessor<'a>.Start(fun agent -> 
+        let rec loop () = async {
+            let! msg = agent.Receive ()
+            onMsg.Invoke msg
+            return! loop ()
+        }
+        loop ()
+    )
+
+    member this.Post(msg) = agent.Post msg
+  
